2a8693193a728ae19270b138f807c6d776507ca2
samirjabib <samirdev9669@gmail.com>
2024-06-24 17:16:38 -0500

    put form for insert data to database

--- src/components/home/table-pickers-form/table-pickers-form.tsx
+++ src/components/home/table-pickers-form/table-pickers-form.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { Label } from "@/components/ui/label";
 import { Button } from "../../ui/button";
+
 import {
   Dialog,
   DialogContent,
@@ -7,6 +11,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../../ui/dialog";
+import { Input } from "@/components/ui/input";
+import { PhoneInput } from "@/components/ui/phone-input";
 
 export default function TablePickersForm() {
   return (
@@ -14,14 +20,38 @@ export default function TablePickersForm() {
       <DialogTrigger>
         <Button>Add Picker</Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="sm:max-w-[425px] w-[95%] rounded-lg">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
+          <DialogTitle>Add picker</DialogTitle>
         </DialogHeader>
+        <div>
+          <Label htmlFor="email">Name</Label>
+          <Input type="email" id="email" placeholder="Insert name" />
+        </div>
+        <div>
+          <Label htmlFor="email">Adrees</Label>
+          <Input type="email" id="email" placeholder="Insert adrees" />
+        </div>
+
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <PhoneInput
+            type="text"
+            id="phone"
+            placeholder="Insert phone"
+            defaultCountry="CO"
+          />
+        </div>
+        <div className="flex flex-row gap-x-4">
+          <div>
+            <Label htmlFor="email">Material name</Label>
+            <Input type="email" id="email" placeholder="Insert material name" />
+          </div>
+          <div>
+            <Label htmlFor="email">Quantity on kg</Label>
+            <Input type="email" id="email" placeholder="Insert quantity" />
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
